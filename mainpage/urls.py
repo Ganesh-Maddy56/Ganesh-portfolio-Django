58bf291ebc078fb5c61d45336dbff28aab7b302c
ganesh <ganeshmaddy354@gamil.com>
2022-01-15 19:36:34 +0530

    updated requrement.txt

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from .views import ProjectListAndFormView
 
 urlpatterns = [
-    url(r'^$', ProjectListAndFormView.as_view(), name='main')
+    url('', ProjectListAndFormView.as_view(), name='main')
 ]
